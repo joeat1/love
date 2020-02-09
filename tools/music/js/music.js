14e290c2612028443af38228178d590fcff4b845
Joe <34472403+joeat1@users.noreply.github.com>
2020-02-09 15:50:51 +0800

    Update music.js

--- tools/music/js/music.js
+++ tools/music/js/music.js
@@ -145,7 +145,7 @@ $(function() {
         var ajax = function ajax(input, filter, type, page) {
           $.ajax({
             type: 'POST',
-            url: https://music.liuzhijin.cn,
+            url: "https://music.liuzhijin.cn",
             timeout: 30000,
             data: {
               input: input,
